a72404ba7d307b421d9b567d7d5fc0c06ed64eab
cha0s <git@cha0s.io>
2024-11-25 23:57:14 -0600

    perf: array codec

--- src/codecs/array.js
+++ src/codecs/array.js
@@ -1,5 +1,27 @@
 import {Codecs} from '../codecs.js';
 
+function typeToElementClass(type) {
+  switch (type) {
+    case 'int8': return Int8Array;
+    case 'uint8': return Uint8Array;
+    case 'int16': return Int16Array;
+    case 'uint16': return Uint16Array;
+    case 'int32': return Int32Array;
+    case 'uint32': return Uint32Array;
+    case 'float32': return Float32Array;
+    case 'float64': return Float64Array;
+  }
+  return undefined;
+}
+
+function paddingForType(type) {
+  let padding = 0;
+  switch (type) {
+    case 'float64': padding = 4; break;
+  }
+  return padding;
+}
+
 class ArrayCodec {
 
   $$elementCodec;
@@ -11,11 +33,22 @@ class ArrayCodec {
     // todo: throw on optional or honor and encode sparse arrays
     // todo: boolean coalescence
     this.$$elementCodec = new Codecs[blueprint.element.type](blueprint.element);
+    this.$$type = blueprint.element.type;
   }
 
   decode(view, target) {
     const length = view.getUint32(target.byteOffset);
     target.byteOffset += 4;
+    let ElementClass = typeToElementClass(this.$$type);
+    if (ElementClass) {
+      const value = new ElementClass(
+        view.buffer,
+        view.byteOffset + target.byteOffset + paddingForType(this.$$type),
+        length,
+      );
+      target.byteOffset += length;
+      return value;
+    }
     const value = Array(length);
     for (let i = 0; i < length; ++i) {
       value[i] = this.$$elementCodec.decode(view, target);
@@ -26,9 +59,20 @@ class ArrayCodec {
   encode(value, view, byteOffset) {
     let length = 0;
     let written = 4;
-    for (const element of value) {
-      length += 1;
-      written += this.$$elementCodec.encode(element, view, byteOffset + written);
+    let ElementClass = typeToElementClass(this.$$type);
+    if (ElementClass && Array.isArray(value)) {
+      length = value.length;
+      new ElementClass(
+        view.buffer,
+        view.byteOffset + byteOffset + written + paddingForType(this.$$type)
+      ).set(new ElementClass(value));
+      written += ElementClass.BYTES_PER_ELEMENT * length;
+    }
+    else {
+      for (const element of value) {
+        length += 1;
+        written += this.$$elementCodec.encode(element, view, byteOffset + written);
+      }
     }
     view.setUint32(byteOffset, length);
     return written;
@@ -36,6 +80,15 @@ class ArrayCodec {
 
   size(value) {
     let size = 4;
+    let ElementClass = typeToElementClass(this.$$type);
+    if (ElementClass) {
+      if (Array.isArray(value)) {
+        return size + paddingForType(this.$$type) + value.length * ElementClass.BYTES_PER_ELEMENT;
+      }
+      if (value instanceof Set) {
+        return size + paddingForType(this.$$type) + value.size * ElementClass.BYTES_PER_ELEMENT;
+      }
+    }
     for (const element of value) {
       size += this.$$elementCodec.size(element);
     }
--- src/codecs/array.test.js
+++ src/codecs/array.test.js
@@ -3,14 +3,52 @@ import {expect, test} from 'vitest';
 import {Codecs} from '../codecs.js';
 import Codec from './array.js';
 import Uint8Codec from './uint8.js';
+import Int8Codec from './int8.js';
+import Int16Codec from './int16.js';
+import Uint16Codec from './uint16.js';
+import Int32Codec from './int32.js';
+import Uint32Codec from './uint32.js';
+import Float32Codec from './float32.js';
+import Float64Codec from './float64.js';
+import StringCodec from './string.js';
 
 Codecs.uint8 = Uint8Codec;
+Codecs.int8 = Int8Codec;
+Codecs.uint8 = Uint8Codec;
+Codecs.int16 = Int16Codec;
+Codecs.uint16 = Uint16Codec;
+Codecs.int32 = Int32Codec;
+Codecs.uint32 = Uint32Codec;
+Codecs.float32 = Float32Codec;
+Codecs.float64 = Float64Codec;
+Codecs.string = StringCodec;
 
-test('array', async () => {
-  const codec = new Codec({
-    element: {type: 'uint8'},
+for (const numberType of [
+  'int8',
+  'uint8',
+  'int16',
+  'uint16',
+  'int32',
+  'uint32',
+  'float32',
+  'float64',
+]) {
+  test(`${numberType} array`, async () => {
+    const codec = new Codec({
+      element: {type: numberType},
+    });
+    const value = [1, 2, 3, 4];
+    const view = new DataView(new ArrayBuffer(codec.size(value)));
+    codec.encode(value, view, 0);
+    expect(Array.from(codec.decode(view, {byteOffset: 0}))).to.deep.equal(value);
   });
-  const value = [1, 2, 3, 4];
+}
+
+test('string array', async () => {
+  const codec = new Codec({
+    element: {type: 'string'},
+  });
+  const value = ['one', 'two', 'three', 'four'];
   const view = new DataView(new ArrayBuffer(codec.size(value)));
   codec.encode(value, view, 0);
   expect(codec.decode(view, {byteOffset: 0})).to.deep.equal(value);
